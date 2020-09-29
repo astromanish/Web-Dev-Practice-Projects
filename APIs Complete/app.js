0710684f6ffb27604c12b270d3848d5f7ab43be4
Manish Singh <astromanishsingh@gmail.com>
2020-09-30 00:28:57 +0600

    useless project removed

--- APIs Complete/app.js
+++
@@ -1,9 +0,0 @@
-axios({
-    method : "get",
-    url : "https://jsonplaceholder.typicode.com/todos",
-    params: {
-        _limit : 10
-    }
-})
-.then(res => console.log(res))
-.catch(err => console.error(err))
--- Practice 4/app.js
+++
@@ -1,9 +0,0 @@
-function myfun()
-{
-    for(let i=0;;i++)
-    {
-        document.body.style.background = radial-gradient( red, blue);
-        document.body.style.background = linear-gradient( blue, red);
-    }
-   
-}
